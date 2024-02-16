8490cd4c40b2a000d694e362b82d7f3484cf1e79
Andre Tsai <andre.tsai23456@gmail.com>
2024-02-16 16:12:04 -0600

    converter done

--- dat_converter.py
+++ dat_converter.py
@@ -15,22 +15,34 @@ def convert(cases):
         lines = []
         # skip first line (headers)
         next(bra_file)
-        # [from_bus, to_bus, id, reactance, lower_line_limit, upper_line_limit]
+        # [from_bus, to_bus, line_id, reactance, lower_line_limit, upper_line_limit]
+        # parse it to [from_bus, to_bus, line_id, bus_susceptance, upper_line_limit]
         max_bus = 1
         max_line_id = 1
         for line in bra_file.readlines():
             parsed_line = line.strip().split(',')
             from_bus, to_bus, line_id, reactance, lower_line_limit, upper_line_limit = parsed_line
+            bus_susceptance = str(int((1/float(reactance))*(10**5)))
+            upper_line_limit = str(int(float(upper_line_limit)*(10**5)))
+            
             max_bus = max(max_bus, int(from_bus), int(to_bus))
             max_line_id = max(max_line_id, int(line_id))
-            lines.append(parsed_line)
+            lines.append([from_bus, to_bus, line_id, bus_susceptance, upper_line_limit])
+            lines.append([to_bus, from_bus, line_id, bus_susceptance, upper_line_limit])
         
+
         demands = []
         # skip first line (headers)
         next(dem_file)
         # [bus, demand]
+        zero_demand_buses = {str(i) for i in range(1,max_bus+1)}
         for line in dem_file.readlines():
-            demands.append(line.strip().split(','))
+            bus, demand = line.strip().split(',')
+            zero_demand_buses.remove(bus)
+            demand = str(int(float(demand)*(10**5)))
+            demands.append([bus, demand])
+        for bus in zero_demand_buses:
+            demands.append([bus, "0"])
 
         generators = []
         # skip first line (headers)
@@ -40,8 +52,11 @@ def convert(cases):
         for line in gen_file.readlines():
             parsed_line = line.strip().split(',')
             bus, gen_id, min_gen, max_gen, lin_cost_coeff = parsed_line
+            min_gen = str(int(float(min_gen)*(10**5)))
+            max_gen = str(int(float(max_gen)*(10**5)))
+            lin_cost_coeff = str(int(float(lin_cost_coeff)*(10**5)))
             max_gen_id = max(max_gen_id, int(gen_id))
-            generators.append(parsed_line)
+            generators.append([bus, gen_id, min_gen, max_gen, lin_cost_coeff])
 
 
         # Writing dat file
@@ -53,8 +68,29 @@ def convert(cases):
         set_init(f, max_line_id)
         f.write("set GENERATOR_IDS :=")
         set_init(f, max_gen_id)
-    
+        f.write("set LINES := ")
+        for from_bus, to_bus, line_id, bus_susceptance, upper_line_limit in lines[:-1]:
+            f.write("("+from_bus+","+to_bus+","+line_id+"), ")
+        f.write("("+lines[-1][0]+","+lines[-1][1]+","+lines[-1][2]+");\n")
+        f.write("set GENERATORS := ")
+        for bus, gen_id, min_gen, max_gen, lin_cost_coeff in generators[:-1]:
+            f.write("("+bus+","+gen_id+"), ")
+        f.write("("+generators[-1][0]+","+generators[-1][1]+");\n\n")
 
+        f.write("param : bus_susceptance\tupper_line_limit\t:=")
+        for from_bus, to_bus, line_id, bus_susceptance, upper_line_limit in lines:
+            f.write("\n"+from_bus+" "+to_bus+" "+line_id+"\t"+bus_susceptance+"\t"+upper_line_limit)
+        f.write(";\n\n")
+
+        f.write("param demand :=")
+        for bus, demand in demands:
+            f.write("\n"+bus+" "+demand)
+        f.write(";\n\n")
+
+        f.write("param : min_generation\tmax_generation\tlinear_cost_coeff\t:=")
+        for bus, gen_id, min_gen, max_gen, lin_cost_coeff in generators:
+            f.write("\n"+bus+" "+gen_id+"\t\t"+min_gen+"\t\t"+max_gen+"\t\t"+lin_cost_coeff)
+        f.write(";\n")
 
 if __name__ == "__main__":
     print("Enter the case names without suffix (Eg. Type in 'case14_ieee'). Press enter to start entering second case name. Enter 'done' and press enter to stop entering case names.")
